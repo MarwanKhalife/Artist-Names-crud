d8216d9f10e70845d9cd4923f8907101e5b13d69
marwankhalife <maljeburi@gmail.com>
2022-07-17 16:52:22 +0200

    changed MongoDB name and link

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 
 let db,
     dbConnectionStr = process.env.DB_STRING,
-    dbName = 'rap'
+    dbName = 'Artist-names'
 
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
